e4fcc4e22ee312c1bdb9678dc13ab4782c214ce4
Tetrafocal <281458187+tetrafocal@users.noreply.github.com>
2026-05-07 19:48:08 -0700

    Minor fixes

--- src/utils/transferPollScheduler.ts
+++ src/utils/transferPollScheduler.ts
@@ -2,7 +2,7 @@
  * Used for polling transfer endpoints, e.g. /downloads and /uploads.
  * We poll at different speeds depending on transfer status, whether there are any files actively downloading or uploading.
  * Due to sampling, we'll sometimes have a snapshot with active transfers followed by a snapshot with only queued files.
- * If we polled immediately after, we'd likely see new files start to download. However, as we'd the snapshot only shows queued files,
+ * If we polled immediately after, we'd likely see new files start to download. However, as the snapshot only shows queued files,
  * we'd would normally slow down to a lower polling rate, delaying UI updates.
  *
  * `transferPollTimer` manages the polling rate, only allowing us to slow down (switch to a larger delay)
@@ -14,7 +14,7 @@ export function transferPollScheduler(
 ) {
   let currentDelay = initialDelay;
 
-  let scheduledDelay: number | undefined = undefined;
+  let scheduledDelay: number | undefined;
   let timer: number | undefined;
 
   function getDelay() {
