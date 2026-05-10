193ab1f79b5c942a346cb6cf4bd53f3e6716f866
Tetrafocal <281458187+tetrafocal@users.noreply.github.com>
2026-05-09 21:28:17 -0700

    Further consolidate Download/Uploads

--- src/components/DownloadPage.tsx
+++ src/components/DownloadPage.tsx
@@ -1,22 +1,16 @@
-import { Component, For, Loading, Match, Show, Switch } from "solid-js";
+import { Component, For, Loading } from "solid-js";
 
 import { getTransfers } from "../services/api/transfer";
 import {
   collateTransferResults,
   TransferGroup,
-  TransferItem,
   TransferResult,
 } from "../services/collateTransferResults";
 import { supportsSortTransfers } from "../utils/apiVersionUtil";
 import { getFolderAndFileName } from "../utils/getFolderAndFileName";
-import { Icon, IconProps } from "./icons/Icon";
-import Check from "./icons/lucide_check.svg";
-import Clock from "./icons/lucide_clock.svg";
+import { Icon } from "./icons/Icon";
 import Folder from "./icons/lucide_folder.svg";
-import Pause from "./icons/lucide_pause.svg";
-import Play from "./icons/lucide_play.svg";
-import Triangle from "./icons/lucide_triangle_alert.svg";
-import { useTransfer } from "./Transfer";
+import { Transfer, TransferGroupItem, useTransfer } from "./Transfer";
 
 import pageStyles from "./Page.module.css";
 import transferStyles from "./Transfer.module.css";
@@ -32,92 +26,26 @@ export const DownloadPage: Component = () => {
       <h1>downloads</h1>
       <Loading>
         <For each={downloads.groups}>
-          {(group) => <DownloadGroupItem group={group()} />}
+          {(group) => (
+            <TransferGroupItem
+              group={group()}
+              header={<DownloadGroupHeader group={group()} />}
+            />
+          )}
         </For>
       </Loading>
     </main>
   );
 };
 
-const DownloadGroupItem: Component<{ group: TransferGroup }> = (props) => {
+const DownloadGroupHeader: Component<{ group: TransferGroup }> = (props) => {
   const sourceParentFolder = () =>
     getFolderAndFileName(props.group.sourcePath)[1];
 
   return (
-    <article class={transferStyles.group}>
-      <header>
-        <h2>
-          <Icon icon={Folder} class={transferStyles.prefixIcon} />/
-          {props.group.username}/<strong>{sourceParentFolder()}</strong>/
-        </h2>
-      </header>
-      <ul>
-        <For each={props.group.items}>
-          {(item) => <Download item={item()} />}
-        </For>
-      </ul>
-    </article>
-  );
-};
-
-const statusIconMap: Record<
-  TransferItem["transferStatus"],
-  Component<IconProps>
-> = {
-  Finished: Check,
-  Paused: Pause,
-  Queued: Clock,
-  Transferring: Play,
-};
-
-const Download: Component<{ item: TransferItem }> = (props) => {
-  const isProbablyErrorStatus = () =>
-    props.item.transferStatus.indexOf(" ") !== -1;
-
-  const paddedProgress = () => props.item.progressPercentage.toFixed(1);
-
-  return (
-    <li class={transferStyles.download}>
-      <div class={transferStyles.trackName} title={props.item.filename}>
-        <Icon
-          icon={statusIconMap[props.item.transferStatus] || Triangle}
-          class={transferStyles.statusIcon}
-        />
-        {props.item.filename}
-      </div>
-      <Show when={props.item.transferStatus !== "Finished"}>
-        {
-          <div class={transferStyles.details}>
-            <Show when={!isProbablyErrorStatus()}>
-              <div class={transferStyles.progressBar}>
-                <div
-                  class={transferStyles.progress}
-                  style={{ width: `${props.item.progressPercentage}%` }}
-                ></div>
-              </div>
-
-              <span>{paddedProgress()}%</span>
-            </Show>
-            <Switch>
-              <Match when={props.item.transferStatus === "Paused"}>
-                <span class={transferStyles.statusText}>Paused</span>
-              </Match>
-              <Match when={props.item.transferStatus === "Queued"}>
-                <span class={transferStyles.statusText}>Queued</span>
-              </Match>
-              <Match when={props.item.transferStatus === "Transferring"}>
-                <span class={transferStyles.statusText}>Downloading</span>
-              </Match>
-              <Match when={isProbablyErrorStatus}>
-                <span class={transferStyles.statusText}>
-                  <strong>{props.item.transferStatus}</strong>
-                </span>
-              </Match>
-            </Switch>
-          </div>
-        }
-      </Show>
-    </li>
+    <>
+      /{props.group.username}/<strong>{sourceParentFolder()}</strong>
+    </>
   );
 };
 
--- src/components/Transfer.tsx
+++ src/components/Transfer.tsx
@@ -1,20 +1,133 @@
 import {
+  Component,
   createEffect,
   createProjection,
+  Element as SolidElement,
   isRefreshing,
+  Match,
   onSettled,
   refresh,
+  Show,
+  Switch,
   useContext,
+  For,
+  createMemo,
 } from "solid-js";
 
 import {
   EmptyTransferResult,
+  TransferGroup,
+  TransferItem,
   TransferResult,
 } from "../services/collateTransferResults";
 import { SettingsStoreContext } from "../stores/SettingsStore";
 import { safeRefresh } from "../utils/safeRefresh";
 import { transferPollScheduler } from "../utils/transferPollScheduler";
 import { useLocalStorage } from "../utils/useStorage";
+import { Icon, IconProps } from "./icons/Icon";
+import Check from "./icons/lucide_check.svg";
+import Clock from "./icons/lucide_clock.svg";
+import Folder from "./icons/lucide_folder.svg";
+import Pause from "./icons/lucide_pause.svg";
+import Play from "./icons/lucide_play.svg";
+import Triangle from "./icons/lucide_triangle_alert.svg";
+
+import styles from "./Transfer.module.css";
+
+export const TransferGroupItem: Component<{
+  group: TransferGroup;
+  header: SolidElement;
+}> = (props) => {
+  const collapseFinishedItems = createMemo(() =>
+    props.group.items.every((item) => item.transferStatus === "Finished"),
+  );
+
+  return (
+    <article class={styles.group}>
+      <h2>
+        <Icon icon={Folder} class={styles.prefixIcon} />
+        {props.header}
+      </h2>
+      <ul>
+        <For each={props.group.items}>
+          {(item) => (
+            <Transfer
+              item={item()}
+              collapseFinishedItems={collapseFinishedItems()}
+            />
+          )}
+        </For>
+      </ul>
+    </article>
+  );
+};
+
+const statusIconMap: Record<
+  TransferItem["transferStatus"],
+  Component<IconProps>
+> = {
+  Finished: Check,
+  Paused: Pause,
+  Queued: Clock,
+  Transferring: Play,
+};
+
+const statusTextMap: Record<TransferItem["transferStatus"], string> = {
+  Paused: "Paused",
+  Queued: "Queued",
+  Transferring: "Downloading",
+};
+
+export const Transfer: Component<{
+  item: TransferItem;
+  collapseFinishedItems: boolean;
+}> = (props) => {
+  const isProbablyErrorStatus = () => props.item.transferStatus.includes(" ");
+  const paddedProgress = () => props.item.progressPercentage.toFixed(1);
+
+  return (
+    <li class={styles.download}>
+      <div class={styles.trackName} title={props.item.filename}>
+        <Icon
+          icon={statusIconMap[props.item.transferStatus] || Triangle}
+          class={styles.statusIcon}
+        />
+        {props.item.filename}
+      </div>
+      <Show
+        when={
+          props.item.transferStatus !== "Finished" ||
+          !props.collapseFinishedItems
+        }
+      >
+        <div class={styles.details}>
+          <Show
+            when={!isProbablyErrorStatus() || props.item.progressPercentage > 0}
+          >
+            <div class={styles.progressBar}>
+              <div
+                class={styles.progress}
+                style={{
+                  "transform-origin": "left",
+                  transform: `scaleX(${props.item.progressPercentage / 100.0})`,
+                  "will-change": "transform",
+                }}
+              ></div>
+            </div>
+            <span>{paddedProgress()}%</span>
+          </Show>
+          <span class={styles.statusText}>
+            <Switch fallback={<strong>{props.item.transferStatus}</strong>}>
+              <Match when={props.item.transferStatus in statusTextMap}>
+                {statusTextMap[props.item.transferStatus]}
+              </Match>
+            </Switch>
+          </span>
+        </div>
+      </Show>
+    </li>
+  );
+};
 
 export type UseTransferArgs = {
   cacheKey: string;
